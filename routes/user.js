0d45fbf6050e3b1715429bbad5956f94a09547d4
Lyubitshka <antwarowska@gmail.com>
2024-01-04 13:38:56 +0100

    routes update

--- routes/user.js
+++ routes/user.js
@@ -3,12 +3,12 @@ const userController = require("../controllers/userController");
 const { verifyAndAuthorize, verifyToken, verifyTokenAndAdmin } = require("../middleware/verifyToken");
 
 // UPDATE USER 
-router.put("/:id", verifyAndAuthorize, userController.updateUser);
+router.put("/", verifyAndAuthorize, userController.updateUser);
 
 // DELETE USER
-router.delete("/:id", verifyAndAuthorize, userController.deleteUser);
+router.delete("/", verifyAndAuthorize, userController.deleteUser);
 
-router.get('/:id', verifyAndAuthorize, userController.getUser );
+router.get('/', verifyAndAuthorize, userController.getUser );
 
 router.get('/', verifyTokenAndAdmin, userController.getAllUsers);
 
