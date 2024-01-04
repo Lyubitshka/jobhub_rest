6c2c01497fd56ba66e67ca61ca3be0cd6d3cc182
Lyubitshka <antwarowska@gmail.com>
2024-01-04 13:42:35 +0100

    middleware update

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
     if (authHeader) {
-        const token = authHeader.split (" ")[1];
+        const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SEC, async (err, user) => {
             if (err) res.status(403).json("Invalid token");
             req.user = user;
